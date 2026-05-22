23b04c5afcebd6b6306fef673dad60538b461cc0
hardik2005ind <HARDIK.MAHAJAN.WORK@GMAIL.COM>
2026-05-23 04:20:27 +0530

    Create main.tsx

---
+++ blue-hue-makeover-main/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return <h1>CRM Working 🚀</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
